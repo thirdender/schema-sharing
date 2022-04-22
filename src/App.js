f03617ff553926703bcc67194e2a451842cce6b1
Robert Rothermel <thirdender@gmail.com>
2022-04-22 09:25:49 -0400

    Initial attempt to filter by Recipe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+// https://schema.org/version/latest/schemaorg-current-https.jsonld
+import data from './schemaorg-current-https.json';
+const { '@graph': graph } = data;
+
+console.dir(graph);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>
+        {graph
+          .filter((item) => {
+            // https://schema.org/Recipe
+            const {
+              'schema:domainIncludes': {
+                '@id': domainIncludes,
+              } = {},
+            } = item;
+            return domainIncludes === 'schema:Recipe';
+          })
+          .map((item) => `${JSON.stringify(item, null, 2)}\n\n`)}
+      </pre>
     </div>
   );
 }
